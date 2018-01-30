97041da80ec3ab52241c1b45a2739cf3de6e77f1
yamunanavada <yamunanavada@gmail.com>
2018-01-30 09:12:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Address from './components/Address';
+import ToggleButton from './components/ToggleButton';
+
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
+  <ToggleButton />,
   document.getElementById('root')
-)
+);
+
+ReactDOM.render(
+  <Address
+    street="Santa Monica Blvd."
+    city="Santa Monica"
+  />,
+  document.getElementById('root')
+);
